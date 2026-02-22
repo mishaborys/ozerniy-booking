1783fea831290f864ad36ed34518a7d2d1c630a2
mishaborys <mishaborys@gmail.com>
2026-02-22 18:32:16 +0200

    make dark as default

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,15 +8,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="uk">
+    <html lang="uk" className="dark">
       <head>
         <Script
           src="https://telegram.org/js/telegram-web-app.js"
           strategy="beforeInteractive"
         />
-        <script dangerouslySetInnerHTML={{ __html: `
-          document.documentElement.classList.add('dark');
-        ` }} />
       </head>
       <body>{children}</body>
     </html>
